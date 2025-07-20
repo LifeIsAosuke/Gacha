a703a69c97f1a0f6c38fba84e30a00360fcdacf9
Aoi-Suzuki <aoidon.lifeistech@gmail.com>
2025-07-20 10:58:24 +0900

    Initial Commit

---
+++ Gacha/GachaApp.swift
@@ -0,0 +1,17 @@
+//
+//  GachaApp.swift
+//  Gacha
+//
+//  Created by A S on 2025/07/20.
+//
+
+import SwiftUI
+
+@main
+struct GachaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GachaTests/GachaTests.swift
@@ -0,0 +1,17 @@
+//
+//  GachaTests.swift
+//  GachaTests
+//
+//  Created by A S on 2025/07/20.
+//
+
+import Testing
+@testable import Gacha
+
+struct GachaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
